8519b44559ad49ced18013194ece78e27ef55eb4
John Swekeny <john.swekenyi@ezra.world>
2023-09-20 13:28:26 +0300

    Updated assertions

--- src/main/java/org/example/TestLogin.java
+++ src/main/java/org/example/TestLogin.java
@@ -6,14 +6,20 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class TestLogin {
     @Test
-    public void openLoginPage() {
+    public void openLoginPage() throws InterruptedException {
         Selenide.open("https://practicetestautomation.com/practice-test-login/");
         $(By.name("username")).setValue("student");
-        $(By.name("password")).setValue("password123");
+        $(By.name("password")).setValue("Password123");
         $(By.xpath("//*[@id=\"submit\"]")).click();
+        Thread.sleep(3000);
+        //Assert the loaded URL after login
+        url().contentEquals("practicetestautomation.com/practice-test-login/");
+        $(By.xpath("//*[@id=\"loop-container\"]/div/article/div[2]/p[1]/strong"))
+                .shouldHave((Condition.text("Congratulations student. You successfully logged in!")));
     }
     @Test
     public void testInvalidLogin() {
